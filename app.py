351395753f35037eddd5a9a939e338c7e775e53c
Harsh Agarwal <77448365+harsh070302@users.noreply.github.com>
2023-07-15 16:21:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,137 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from datetime import datetime as dt
+import yfinance as yf
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.express as px
+# model
+from model import prediction
+from sklearn.svm import SVR
+
+
+def get_stock_price_fig(df):
+
+    fig = px.line(df,
+                  x="Date",
+                  y=["Close", "Open"],
+                  title="Closing and Openning Price vs Date")
+
+    return fig
+
+
+
+
+app = dash.Dash(
+    __name__,
+    external_stylesheets=[
+        "https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+    ])
+server = app.server
+
+# layout 
+app.layout = html.Div(
+    [
+        html.Div(
+            [
+                # Nav
+                html.P("Welcome to Stock Visualization And Forecasting", className="start"),
+                html.Div([
+                    html.P("Input stock code of Company : "),
+                    html.Div([
+                        dcc.Input(id="dropdown_tickers", type="text" ),
+                        html.Button("Submit", id='submit'),
+                    ],
+                             className="form")
+                ],
+                         className="input-place"),
+                html.Div([
+                    dcc.DatePickerRange(id='my-date-picker-range',
+                                        min_date_allowed=dt(1995, 8, 5),
+                                        max_date_allowed=dt.now(),
+                                        initial_visible_month=dt.now(),
+                                        end_date=dt.now().date()),
+                ],
+                         className="date"),
+                html.Div([
+                    html.Button(
+                        "Stock Price", className="stock-btn", id="stock"),
+                   
+                    dcc.Input(id="n_days",
+                              type="text",
+                              placeholder="number of days"),
+
+                    html.Button(
+                        "Forecast", className="forecast-btn", id="forecast")
+                ],
+                         className="buttons"),
+                
+            ],
+            className="nav"),
+
+        # content
+        html.Div(
+            [
+                html.Div(
+                   [ 
+                    ],
+                    className="header"),
+               
+                html.Div([], id="graphs-content"),
+                html.Div([], id="main-content"),
+                html.Div([], id="forecast-content")
+            ],
+            className="content"),
+    ],
+    className="container")
+
+
+
+
+# callback for visualization
+@app.callback([
+    Output("graphs-content", "children"),
+], [
+    Input("stock", "n_clicks"),
+    Input('my-date-picker-range', 'start_date'),
+    Input('my-date-picker-range', 'end_date')
+], [State("dropdown_tickers", "value")])
+def stock_price(n, start_date, end_date, val):
+    if n == None:
+        return [""]
+        
+    if val == None:
+        raise PreventUpdate
+    else:
+        if start_date != None:
+            df = yf.download(val, str(start_date), str(end_date))
+        else:
+            df = yf.download(val)
+
+    df.reset_index(inplace=True)
+    fig = get_stock_price_fig(df)
+    return [dcc.Graph(figure=fig)]
+
+
+
+
+
+# callback for forecast
+@app.callback([Output("forecast-content", "children")],
+              [Input("forecast", "n_clicks")],
+              [State("n_days", "value"),
+               State("dropdown_tickers", "value")])
+def forecast(n, n_days, val):
+    if n == None:
+        return [""]
+    if val == None:
+        raise PreventUpdate
+    fig = prediction(val, int(n_days) + 1)
+    return [dcc.Graph(figure=fig)]
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
